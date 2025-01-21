fc424d1ac0937d4527950a30db2faee7717e8020
luizfaria1989 <luizguilermemoraiscosta@gmail.com>
2025-01-21 09:08:03 -0300

    implementacao dos PopUps nas paginas de camiseta

--- my-react-app/src/pages/Camisetas/Camiseta1.jsx
+++ my-react-app/src/pages/Camisetas/Camiseta1.jsx
@@ -1,17 +1,75 @@
-import { Header, FooterDeVendas, MenuCamiseta, BotaoWhatsapp } from '../../components/index.js';
+import {useState} from 'react';
+import { Header, FooterDeVendas, MenuCamiseta, BotaoWhatsapp, BotaoEditarSecao, PopUpEditarProduto } from '../../components/index.js';
 import styles from './Camiseta.module.css'
+import { useConteudos } from "../../conteudos.js";
 
 function PaginaCamiseta1() {
+
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [dadosEdicao, setDadosEdicao] = useState({tipo: null});
+
+  const {
+    produtos,
+    setProdutos,
+  } = useConteudos();
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode)
+
+  }
+
+  const fecharPopUp = () => {
+    setIsEditMode(false);
+    setDadosEdicao({tipo: null});
+  };
+
+  const salvarEdicao = (dadosEditados) => {
+    console.log("Dados salvos:", dadosEditados);
+    const { conteudo } = dadosEditados;
+    setConteudoSecoes((prev) => ({
+      ...prev,
+    }));
+  };
+
+  function abrirEdicaoProduto(dados) {
+    setDadosEdicao({ ...dados, tipo: 'produto' });
+    setIsEditMode(true);
+  }
+
 
   return(
 
     <div className={styles.PaginaCamiseta}>
-      <Header/>
+      <Header isEditMode={isEditMode} toggleEditMode={toggleEditMode}/>
 
       <MenuCamiseta
-        imgSrc='/assets/Camisetas/CamisetaMCLaren.png'
-        nome = "RIVALS COLLECTIONS 2024 | MCLAREN"
-        preco= "R$ 59,99"/>
+        imgSrc= {produtos.camisetaMCLaren.imgFrente}
+        nome = {produtos.camisetaMCLaren.nome}
+        preco= {produtos.camisetaMCLaren.preco}/>
+
+      {isEditMode &&(
+        <>
+          <BotaoEditarSecao className={styles.BotaoEditar}
+          onClick={() => 
+            abrirEdicaoProduto({
+              imgFrente: produtos.camisetaMCLaren.imgFrente,
+              altFrente: produtos.camisetaMCLaren.altFrente,
+              imgTras: produtos.camisetaMCLaren.imgTras,
+              altTras: produtos.camisetaMCLaren.altTras,
+              nome: produtos.camisetaMCLaren.nome,
+              preco: produtos.camisetaMCLaren.preco,
+            })
+          }
+          />
+        </>
+      )}
+      {isEditMode && dadosEdicao.tipo === 'produto' &&(
+        <PopUpEditarProduto
+          dados={dadosEdicao}
+          onClose={fecharPopUp}
+          onSave={salvarEdicao}
+        />
+      )}
 
       <BotaoWhatsapp/>
   
--- my-react-app/src/pages/Camisetas/Camiseta2.jsx
+++ my-react-app/src/pages/Camisetas/Camiseta2.jsx
@@ -1,17 +1,73 @@
-import { Header, FooterDeVendas, MenuCamiseta, BotaoWhatsapp } from '../../components/index.js';
+import {useState} from 'react';
+import { Header, FooterDeVendas, MenuCamiseta, BotaoWhatsapp, BotaoEditarSecao, PopUpEditarProduto } from '../../components/index.js';
 import styles from './Camiseta.module.css'
+import {useConteudos} from '../../conteudos.js'
 
 function PaginaCamiseta2() {
+
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [dadosEdicao, setDadosEdicao] = useState({tipo: null});
+
+  const {
+    produtos,
+    setProdutos,
+  } = useConteudos();
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode)
+  }
+
+  const fecharPopUp = () => {
+    setIsEditMode(false);
+    setDadosEdicao({tipo: null});
+  };
+
+  const salvarEdicao = (dadosEditados) => {
+    console.log("Dados salvos:", dadosEditados);
+    const { conteudo } = dadosEditados;
+    setConteudoSecoes((prev) => ({
+      ...prev,
+    }));
+  };
+
+  function abrirEdicaoProduto(dados) {
+    setDadosEdicao({ ...dados, tipo: 'produto' });
+    setIsEditMode(true);
+  }
 
   return(
 
     <div className={styles.PaginaCamiseta}>
-      <Header/>
+      <Header isEditMode={isEditMode} toggleEditMode={toggleEditMode}/>
 
       <MenuCamiseta
-        imgSrc='/assets/Camisetas/CamisetaRedbull.png'
-        nome = "RIVALS COLLECTIONS 2024 | MCLAREN"
-        preco= "R$ 59,99"/>
+        imgSrc={produtos.camisetaRedBull.imgFrente}
+        nome = {produtos.camisetaRedBull.nome}
+        preco={produtos.camisetaRedBull.preco}/>
+
+      {isEditMode &&(
+              <>
+                <BotaoEditarSecao className={styles.BotaoEditar}
+                onClick={() => 
+                  abrirEdicaoProduto({
+                    imgFrente: produtos.camisetaRedBull.imgFrente,
+                    altFrente: produtos.camisetaRedBull.altFrente,
+                    imgTras: produtos.camisetaRedBull.imgTras,
+                    altTras: produtos.camisetaRedBull.altTras,
+                    nome: produtos.camisetaRedBull.nome,
+                    preco: produtos.camisetaRedBull.preco,
+                  })
+                }
+                />
+              </>
+            )}
+            {isEditMode && dadosEdicao.tipo === 'produto' &&(
+              <PopUpEditarProduto
+                dados={dadosEdicao}
+                onClose={fecharPopUp}
+                onSave={salvarEdicao}
+              />
+            )}
 
       <BotaoWhatsapp/>
   
