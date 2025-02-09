6ed6d27de4a928d6c2d0325b5b0766cb93bf0934
Miguel <miguelpg.programacao@gmail.com>
2025-02-09 02:15:06 -0300

    alteracao do texto na pagina historia, adiçao de membros da equipe(commit feito junto com o tonin)

--- my-react-app/src/pages/Historia/Historia.jsx
+++ my-react-app/src/pages/Historia/Historia.jsx
@@ -2,26 +2,68 @@ import { Header, Footer, Titulo, ParagrafoPadrao } from '../../components/index.
 import styles from './Historia.module.css';
 
 function Historia() {
-  return(
+  const membros = [
+    "Arthur da Paz Magalhães - Engenharia Mecânica",
+    "Artur Henrique dos Santos Oliveira - Engenharia Automotiva",
+    "Bruno Maurício Neves Viar - Engenharia Automotiva",
+    "Carlos Eduardo Meireles do Amaral - Engenharia Aeroespacial",
+    "Daniel Ribeiro Félix - Engenharia de Energia",
+    "Danilo Ligiero - Engenharia Aeroespacial",
+    "Davi da Paz Magalhães - Engenharia Automotiva",
+    "Diego Sampaio Tebaldi - Engenharias",
+    "Eduardo Schuindt Santos - Engenharia de Software",
+    "Enzo Govatski - Engenharia Automotiva",
+    "Felipe Andrade - Engenharia Eletrônica",
+    "Felipe Sidrane - Engenharia Eletrônica",
+    "Gabriel Basto Bertolazi - Engenharia de Software",
+    "Gabriel Barges - Engenharia Automotiva",
+    "Gabriel de Avelar Leite - Engenharia Automotiva",
+    "Giavana Martins de Brito - Engenharia de Software",
+    "Guilherme Rodrigues de Carvalho - Engenharia Automotiva",
+    "Gustavo Santos da Silva Resende - Engenharia de Energia",
+    "Italo Bezerra - Engenharia Aeroespacial",
+    "João Carlos Labo Sousa Monteira - Engenharia de Software",
+    "João Guilherme Nascimento Lourenço - Engenharia de Energia",
+    "João Victor Marques Reis de Miranda - Engenharia de Software",
+    "Joed dos Santos de Oliveira - Engenharia Automotiva",
+    "Jardano dos Santos de Oliveira - Engenharia Eletrônica",
+    "Lavynia Fátima da Silva Sousa - Engenharia de Energia",
+    "Lucas Mateus Teixeira de Souza - Engenharia de Software",
+    "Luiz Gabriel Lima Oliveira - Engenharia Automotiva",
+    "Mateus Fidelis Marinho Maia - Engenharia de Software",
+    "Otávio Augusto Binder - Engenharia Eletrônica",
+    "Paulo Caleb Fernandes da Silva - Engenharia Eletrônica",
+    "Paulo Emanuel Mendes Amorim - Engenharia Automotiva",
+    "Paulo Fernandes de Melo - Engenharia Automotiva",
+    "Rafael Sobreira Maura - Engenharia Automotiva",
+    "Rafael Torres Gonzaga Parreira - Engenharia de Software",
+    "Renan Camara de Oliveira Farias - Engenharia Eletrônica",
+    "Tasso Oliveira da Rocha - Engenharia Eletrônica",
+    "Thalis Cezar Ianzner - Engenharia Eletrônica"
+  ];
+
+  return (
     <div className={styles.PaginaHistoria}>
-      <Header/>
+      <Header />
 
       <div className={styles.SecaoNossaHistoria}>
         <div className={styles.TextoSecao}>
-          <Titulo texto="Nossa História" tamanho="86px" gradiente={false}/>
-
-          <ParagrafoPadrao texto="A Fórmula Gama Racing (FGR) é a primeira e única equipe de Fórmula SAE Elétrico do Centro-Oeste, fundada em 30 de outubro há 11 anos na Universidade de Brasília (UnB). Sua missão é impulsionar a eletromobilidade sustentável no Distrito Federal. A equipe já desenvolveu cinco protótipos elétricos, e atualmente trabalha no modelo E05, que incorpora avanços significativos em mobilidade elétrica e aerodinâmica."/>
-
-          <ParagrafoPadrao texto="A FGR conta com aproximadamente 47 membros divididos entre trainees e efetivos. A equipe possui departamentos técnicos (Mecânica e Elétrica) e administrativos (Gestão de Pessoas, Marketing, Comercial, Financeiro/Logística), sendo liderada por um Capitão, um Gerente de Projetos e uma Gerente de Departamentos."/>
-
-          <ParagrafoPadrao texto="A FGR participa anualmente da Fórmula SAE Brasil, em São Paulo. Entre os principais resultados estão: em 2023 a equipe ficou em 11º lugar, em 2022 foi 8º lugar, em 2019 conquistou o 12º lugar, e em 2018 e 2017 ficou com o 13º lugar."/>
-
-          <ParagrafoPadrao texto="Atualmente, a equipe está finalizando o desenvolvimento do E05, um protótipo com tecnologias de ponta. Além das competições, a FGR promove a eletromobilidade sustentável em eventos como congressos de engenharia, Campus Party, C-Move, e realiza iniciativas inclusivas, como o projeto 'Mecânica para Mulheres'."/>
-
+          <Titulo texto="Nossa História" tamanho="86px" gradiente={false} />
+          <ParagrafoPadrao texto="A Fórmula Gama Racing (FGR) é a primeira e única equipe de Fórmula SAE Elétrico do Centro-Oeste, fundada em 30 de outubro há 11 anos na Universidade de Brasília (UnB). Sua missão é impulsionar a eletromobilidade sustentável no Distrito Federal. A equipe já desenvolveu cinco protótipos elétricos, e atualmente trabalha no modelo E05, que incorpora avanços significativos em mobilidade elétrica e aerodinâmica." />
+          <ParagrafoPadrao texto="A FGR conta com aproximadamente 47 membros divididos entre trainees e efetivos. A equipe possui departamentos técnicos (Mecânica e Elétrica) e administrativos (Gestão de Pessoas, Marketing, Comercial, Financeiro/Logística), sendo liderada por um Capitão, um Gerente de Projetos e uma Gerente de Departamentos." />
+          <ParagrafoPadrao texto="A equipe já participou de diversas competições nacionais e internacionais, como a Fórmula SAE Brasil e a competição Formula Student, destacando-se pela inovação e performance de seus protótipos. A FGR busca, a cada edição, elevar o nível de seus veículos e consolidar a presença da universidade no cenário da engenharia automotiva sustentável." />
+          
+          <Titulo texto="Membros e seus Cursos" tamanho="48px" gradiente={false} />
+          
+          <div className={styles.MembrosContainer}>
+            {membros.map((membro, index) => (
+              <span key={index} className={styles.MembroItem}>{membro}</span>
+            ))}
+          </div>
         </div>
       </div>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
