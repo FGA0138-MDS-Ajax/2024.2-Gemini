019e92007e9f8b32c34ad52bef00154693abc9aa
Miguel <miguelpg.programacao@gmail.com>
2025-02-09 16:18:46 -0300

    alteracao do texto na pagina patrocinadores\inclusão das imagens com tonin

--- my-react-app/src/pages/Patrocinadores/Patrocinadores.jsx
+++ my-react-app/src/pages/Patrocinadores/Patrocinadores.jsx
@@ -1,31 +1,56 @@
 import { Header, Footer, Titulo } from '../../components/index.js';
-import styles from './Patrocinadores.module.css'
+import styles from './Patrocinadores.module.css';
+
+// Importando as imagens manualmente
+import ansys from '../../assets/icones/patrocinadores/Ansys.jpg';
+import circuibras from '../../assets/icones/patrocinadores/Circuibras.jpg';
+import dassault from '../../assets/icones/patrocinadores/dassautsistems.jpg';
+import finatec from '../../assets/icones/patrocinadores/finatec.jpg';
+import ieee from '../../assets/icones/patrocinadores/IEEEpatrocinadores.jpg';
+import mathworks from '../../assets/icones/patrocinadores/mathworks.jpg';
+import redelease from '../../assets/icones/patrocinadores/redelease.jpg';
+import tesla from '../../assets/icones/patrocinadores/tesla.jpg';
+import texas from '../../assets/icones/patrocinadores/texasinstruments.jpg';
+import weg from '../../assets/icones/patrocinadores/weg.jpg';
+
+const patrocinadores = [
+  { src: ansys, alt: "Ansys" },
+  { src: circuibras, alt: "Circuibras" },
+  { src: dassault, alt: "Dassault Systèmes" },
+  { src: finatec, alt: "Finatec" },
+  { src: ieee, alt: "IEEE" },
+  { src: mathworks, alt: "MathWorks" },
+  { src: redelease, alt: "Redelease" },
+  { src: tesla, alt: "Tesla" },
+  { src: texas, alt: "Texas Instruments" },
+  { src: weg, alt: "WEG" }
+];
 
 function Patrocinadores() {
-
-  return(
-
+  return (
     <section className={styles.Patrocinadores}>
-        <Header/>
+      <Header />
 
-        <div className={styles.SecaoPatrocinadores}>
+      <div className={styles.SecaoPatrocinadores}>
+        <Titulo texto="Nossos Patrocinadores" tamanho="86px" gradiente={false} />
 
-            <Titulo texto="Nossos Patrocinadores" tamanho="86px" gradiente={false}/>
+        <div className={styles.Divisoria} />
 
-            <div className={styles.Divisoria}/>
-
-            <div className={styles.GaleriaPatrocinadores}>
-
-                <img src='assets/Patrocinadores/Dassault_Systemes-Logo.svg' alt="" />
-
-            </div>
-
+        <div className={styles.GaleriaPatrocinadores}>
+          {patrocinadores.map((patrocinador, index) => (
+            <img 
+              key={index} 
+              src={patrocinador.src} 
+              alt={patrocinador.alt} 
+              className={styles.ImagemPatrocinador}
+            />
+          ))}
         </div>
-  
-        <Footer/>
+      </div>
 
+      <Footer />
     </section>
-  )
+  );
 }
 
-export default Patrocinadores
+export default Patrocinadores;
